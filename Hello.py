d16822346e8f81cb49cf9e3bd87cd9d03067a17b
Nasser-Elsaman-94 <153395617+Nasser-Elsaman-94@users.noreply.github.com>
2023-12-14 14:02:15 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -157,3 +157,10 @@ text-align: center;
 </div>
 """
 st.markdown(footer,unsafe_allow_html=True)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
