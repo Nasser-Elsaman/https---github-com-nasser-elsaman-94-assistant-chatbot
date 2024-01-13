3d98701a8c83db130f7ca33f328461bf7332dea0
Nasser-Elsaman-94 <153395617+Nasser-Elsaman-94@users.noreply.github.com>
2024-01-13 16:48:46 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -48,7 +48,7 @@ def radar_chart(personality_prediction):
     fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True, facecolor='white'))
     
     # Set background color to white
-    ax.plot(angles, values, color='blue', linewidth=2, linestyle='solid')
+    ax.plot(angles, values, color='blue', linewidth=9, linestyle='solid')
     ax.fill(angles, values, color='blue', alpha=0.4)
     
     # Add radial gridlines
@@ -59,8 +59,8 @@ def radar_chart(personality_prediction):
     # Add range numbers on the radar chart
     range_numbers = np.linspace(0, 1, 5)
     ax.set_yticks(range_numbers)
-    ax.set_yticklabels(range_numbers, color='black') # Set range numbers color to black
-    # f"{num:.1%} [for num in "
+    ax.set_yticklabels(# f"{num:.1%} [for num in range_numbers], color='black') # Set range numbers color to black
+    
     # Remove the outer box (spines)
     ax.spines['polar'].set_visible(False)
     plt.title("Personality Traits Radar Chart", size=16, color='black', y=1.1) # Set title color to black
