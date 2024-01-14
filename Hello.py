9f2feaf3586961314deb3c6b721e76b5eae0722d
Nasser-Elsaman-94 <153395617+Nasser-Elsaman-94@users.noreply.github.com>
2024-01-14 15:42:35 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -18,7 +18,7 @@ client = gspread.authorize(creds)
 spreadsheet = client.open("Streamlit ML Personality Assessment")  # Replace with your spreadsheet name
 
 # Sidebar
-st.sidebar.title("<span style='color:#FF4B4B'>Configuration</span>", unsafe_allow_html=True)
+st.sidebar.title("<span style='color:#FF4B4B'>Configuration</span>")
 
 def model_callback():
     st.session_state["model"] = st.session_state["model_selected"]
