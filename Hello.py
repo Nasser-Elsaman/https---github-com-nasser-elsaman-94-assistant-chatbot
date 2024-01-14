fdec5485bd6e55603f107511ec7b7d10439abe8e
Nasser-Elsaman-94 <153395617+Nasser-Elsaman-94@users.noreply.github.com>
2024-01-14 09:29:37 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -76,7 +76,7 @@ def radar_chart(personality_prediction):
     # Remove the outer box (spines)
     ax.spines['polar'].set_visible(False)
     
-    plt.title("Personality Traits Radar/ Spider Web Chart \u2744 \u2745 \u2746 \u2747 \u2748 \u2749", size=16, color='black', y=1.1) # Set title color to black
+    plt.title("Personality Traits Radar/ Spider Web Chart \u2745 \u270F \u2713", size=16, color='black', y=1.1) # Set title color to black
     
     st.pyplot(fig)
 
