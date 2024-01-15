5517fa74974406496bcb9c43534aecd6af70924e
Nasser-Elsaman-94 <153395617+Nasser-Elsaman-94@users.noreply.github.com>
2024-01-15 16:33:38 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -235,7 +235,7 @@ if selected == "About":
     st.write (":house_with_garden: Addresses:- :one: El Eshreen Street, ElSalam Area, Cairo Governorate, Egypt. :two: El Mostaqbal Area, Faisal District, Suez Governorate, Egypt.")
     st.write(":globe_with_meridians: Website:- https://elsamaninfo.wordpress.com.")
     st.divider()
-    st.write(":bookmark_tabs: Kindly note that this project of the third one from total 3 projects for researching purposes only and If you need to check the other 2 projects.")
+    st.write(":bookmark_tabs: Kindly note that this project of the third one from total 3 projects for researching purposes only and If you need to check the other 2 projects:-")
     Google_Form_link = "https://docs.google.com/forms/d/e/1FAIpQLSd6Cqq1uknZ27wMYVZsYhIu0asUz5sI4WQ8m4sXHKsdWlqfUA/viewform?pli=1"
     
     st.write(":one: Rule-based Personality Assessment Using Google Form and Google Script (IF-Then Rule) and The data stored in a Google Sheet for researching purposes only [link.](%s)" % Google_Form_link)
